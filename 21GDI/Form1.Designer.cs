688ee81655d44ee8767ba707f8ea95195b41485b
成笑笑 <260714659@qq.com>
2017-04-07 21:33:22 +0800

    简单验证码.0

--- 21GDI/Form1.Designer.cs
+++ 21GDI/Form1.Designer.cs
@@ -28,12 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnDrawLine = new System.Windows.Forms.Button();
+            this.btnDrawRect = new System.Windows.Forms.Button();
+            this.btnDrawLetter = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnDrawLine
+            // 
+            this.btnDrawLine.Location = new System.Drawing.Point(51, 36);
+            this.btnDrawLine.Name = "btnDrawLine";
+            this.btnDrawLine.Size = new System.Drawing.Size(75, 23);
+            this.btnDrawLine.TabIndex = 0;
+            this.btnDrawLine.Text = "绘制直线";
+            this.btnDrawLine.UseVisualStyleBackColor = true;
+            this.btnDrawLine.Click += new System.EventHandler(this.btnDrawLine_Click);
+            // 
+            // btnDrawRect
+            // 
+            this.btnDrawRect.Location = new System.Drawing.Point(188, 35);
+            this.btnDrawRect.Name = "btnDrawRect";
+            this.btnDrawRect.Size = new System.Drawing.Size(75, 23);
+            this.btnDrawRect.TabIndex = 1;
+            this.btnDrawRect.Text = "绘制长方形";
+            this.btnDrawRect.UseVisualStyleBackColor = true;
+            this.btnDrawRect.Click += new System.EventHandler(this.btnDrawRect_Click);
+            // 
+            // btnDrawLetter
+            // 
+            this.btnDrawLetter.Location = new System.Drawing.Point(51, 100);
+            this.btnDrawLetter.Name = "btnDrawLetter";
+            this.btnDrawLetter.Size = new System.Drawing.Size(75, 23);
+            this.btnDrawLetter.TabIndex = 2;
+            this.btnDrawLetter.Text = "绘制文字";
+            this.btnDrawLetter.UseVisualStyleBackColor = true;
+            this.btnDrawLetter.Click += new System.EventHandler(this.btnDrawLetter_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(163, 166);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(495, 293);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnDrawLetter);
+            this.Controls.Add(this.btnDrawRect);
+            this.Controls.Add(this.btnDrawLine);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnDrawLine;
+        private System.Windows.Forms.Button btnDrawRect;
+        private System.Windows.Forms.Button btnDrawLetter;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- 21GDI/Form1.cs
+++ 21GDI/Form1.cs
@@ -15,5 +15,89 @@ namespace _21GDI
         {
             InitializeComponent();
         }
+
+        private void btnDrawLine_Click(object sender, EventArgs e)
+        {
+            //创建GDI+对象
+            Graphics g = this.CreateGraphics();
+
+            //创建Pen对象，参数为   笔的颜色
+            Pen p = new Pen(Brushes.Green);
+
+            Point p1 = new Point(50, 50);
+            Point p2 = new Point(200, 200);
+
+            //DrawLine参数：
+            //1.Pen对象
+            //2.开始的点Point对象
+            //3.结束的点Point对象
+            g.DrawLine(p, p1, p2);
+        }
+
+        private void btnDrawRect_Click(object sender, EventArgs e)
+        {
+            Graphics g = this.CreateGraphics();
+
+            Pen pen = new Pen(Brushes.Red);
+
+            //矩形对象，参数对象：
+            //1.X轴的坐标
+            //2.Y轴的坐标
+            //3.矩形的宽度
+            //4.举行的长度
+            Rectangle r = new Rectangle(100, 100, 100, 100);
+            //绘制矩形
+            g.DrawRectangle(pen, r);
+        }
+
+        private void btnDrawLetter_Click(object sender, EventArgs e)
+        {
+            Graphics g = this.CreateGraphics();
+
+            //参数：
+            //1.绘制的文字
+            //2.字体：设置字体和大小，以及其他样式
+            //3.绘制的点
+            g.DrawString("做全栈攻城狮", new Font("楷体", 20), Brushes.Red, new Point(150, 100));
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = null;
+
+            string str = string.Empty;
+
+            Random rand = new Random();
+
+            for (int i = 0; i < 5; i++)
+            {
+                int num =rand.Next(0, 10);
+                str += num;
+            }
+
+            Bitmap bitmap = new Bitmap(150,40);//创建BitMap  150X40的
+            Graphics g = Graphics.FromImage(bitmap);//直接画到Bitmap上
+
+
+            for (int i = 0; i < 5; i++)
+            {
+                Point p = new Point(i * 20, 0);//指定位置，左上角
+                Color[] colors = new Color[] { Color.Green,Color.Red,Color.Purple,Color.RoyalBlue};
+
+                g.DrawString(str[i].ToString(), new Font("楷体", 20, FontStyle.Bold), new SolidBrush(colors[rand.Next(0, 4)]), p);
+            }
+
+
+            for (int i = 0; i < 20; i++)
+            {
+                //根据Bitmap的高和款，随机画线
+                Point p1 = new Point(rand.Next(0, bitmap.Width), rand.Next(0, bitmap.Height));
+                Point p2 = new Point(rand.Next(0, bitmap.Width), rand.Next(0, bitmap.Height));
+                g.DrawLine(new Pen(Brushes.Green), p1, p2);
+            }
+
+            pictureBox1.Image = bitmap;
+
+        }
     }
 }
